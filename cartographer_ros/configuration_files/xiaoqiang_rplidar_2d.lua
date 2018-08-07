e843cbbbdf83d4b579ad766eb968637f6e80673b
xiaoqiang <xiaoqiang@bwbot.org>
2018-08-07 19:44:00 +0800

    fix rplidar luma parmas

--- cartographer_ros/configuration_files/xiaoqiang_rplidar_2d.lua
+++ cartographer_ros/configuration_files/xiaoqiang_rplidar_2d.lua
@@ -44,14 +44,14 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.laser_min_range = 0.15
-TRAJECTORY_BUILDER_2D.laser_max_range = 16.
-TRAJECTORY_BUILDER_2D.laser_missing_echo_ray_length = 5.
+TRAJECTORY_BUILDER_2D.min_range = 0.15
+TRAJECTORY_BUILDER_2D.max_range = 16.
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
-SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.65
-SPARSE_POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
 return options
