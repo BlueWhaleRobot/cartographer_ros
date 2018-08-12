9518740318fb5c923783b254208808d531fb6bd2
xiaoqiang <xiaoqiang@bwbot.org>
2018-08-12 22:00:49 +0800

    add lungu rslidar support

--- cartographer_ros/configuration_files/assets_writer_xiaoqiang_3d.lua
+++ cartographer_ros/configuration_files/assets_writer_xiaoqiang_3d.lua
@@ -17,12 +17,12 @@ VOXEL_SIZE = 5e-2
 include "transform.lua"
 
 options = {
-  tracking_frame = "base_link",
+  tracking_frame = "laserbase_link",
   pipeline = {
     {
       action = "min_max_range_filter",
       min_range = 1.,
-      max_range = 60.,
+      max_range = 20.,
     },
     {
       action = "dump_num_points",
@@ -62,13 +62,8 @@ options = {
     -- laser.
     {
       action = "color_points",
-      frame_id = "horizontal_vlp16_link",
+      frame_id = "rslidar",
       color = { 255., 0., 0. },
-    },
-    {
-      action = "color_points",
-      frame_id = "vertical_vlp16_link",
-      color = { 0., 255., 0. },
     },
 
     {
